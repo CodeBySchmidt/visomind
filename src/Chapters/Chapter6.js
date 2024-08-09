6e19168f96315e020349927d3b4e1cfb78239997
Anne Schmidt <anne_kathrin.schmidt@smail.th-koeln.de>
2024-08-09 13:04:34 +0200

    Die ersten 6 Chapters eingefügt und grob gestyled!
    -> Sehen noch nicht 100% aus wie in Adobe XD

---
+++ src/Chapters/Chapter6.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Chapter6.css"
+
+
+function Chapter6() {
+
+    return (
+        <div className="Chapter6">
+
+            <div className="ImageContainer">
+                <div className="Paceholder"></div>
+            </div>
+
+            <div className="Container">
+                <div className="Topic">
+                    PERFORMANCE
+                </div>
+                <div className="Content">
+                    <div className="TextColumn">
+                        <p>
+                            Machine learning is a field of study in the bigger term of artificial intelligence which
+                            itself is a field of study in computer science. It concerns itself with the development and
+                            study
+                            of statistical algorithms that can learn from data and generalize to unseen data and thus
+                            perform
+                            tasks without explicit instructions. Within Machine learning we also distinguish between 3
+                            categories
+                        </p>
+                    </div>
+                    <div className="ButtonContainer">
+                        <button type="button">LEARN MORE</button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default Chapter6;
--- src/Components/ScrollAnimation.js
+++ src/Components/ScrollAnimation.js
@@ -8,9 +8,9 @@ import "./ScrollAnimation.css"; // Ihre CSS-Datei für Animationen
 import Chapter1 from "../Chapters/Chapter1";
 import Chapter2 from "../Chapters/Chapter2";
 import Chapter3 from "../Chapters/Chapter3";
-// import Chapter4 from "../Chapters/Chapter4";
-// import Chapter5 from "../Chapters/Chapter5";
-// import Chapter6 from "../Chapters/Chapter6";
+import Chapter4 from "../Chapters/Chapter4";
+import Chapter5 from "../Chapters/Chapter5";
+import Chapter6 from "../Chapters/Chapter6";
 // import Chapter7 from "../Chapters/Chapter7";
 // import Chapter8 from "../Chapters/Chapter8";
 
@@ -63,6 +63,24 @@ const ScrollAnimation = () => {
                     ref={(el) => (elementsRef.current[2] = el)}
                     className="fade-element slide-out">
                     <Chapter3/>
+                </div>
+
+                <div
+                    ref={(el) => (elementsRef.current[3] = el)}
+                    className="fade-element slide-out">
+                    <Chapter4/>
+                </div>
+
+                <div
+                    ref={(el) => (elementsRef.current[4] = el)}
+                    className="fade-element slide-out">
+                    <Chapter5/>
+                </div>
+
+                <div
+                    ref={(el) => (elementsRef.current[5] = el)}
+                    className="fade-element slide-out">
+                    <Chapter6/>
                 </div>
             </main>
         </div>
